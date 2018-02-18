6c8a67e7d55f46f519882ed665111d2814947a36
travis88 <hellsballs99@gmail.com>
2018-02-18 20:13:37 +0300

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname + "/dist/assets",
+        filename: "bundle.js",
+        sourceMapFilename: "bundle.map"
+    },
+    devtool: "#source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'stage-0', 'react']
+                }
+            }
+        ]
+    }
+}
